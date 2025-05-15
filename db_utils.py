f3d2e714078b32d998b1e3e3c2cb4a8ec9751f27
Erprabhat <vkc842396@gmail.com>
2025-05-15 12:23:21 +0530

    added new update

--- db_utils.py
+++ db_utils.py
@@ -31,3 +31,27 @@ def run_bin_query():
     except Exception as e:
         return []
 
+def run_processor_query():
+    query = """
+        SELECT DISTINCT t.processor_name
+        FROM public.altitude_transaction t
+        LEFT JOIN public.altitude_project p ON t.project_id = p.project_id
+        LEFT JOIN public.altitude_customers c ON t.txid = c.txid
+        WHERE EXTRACT(YEAR FROM t.created_date) = EXTRACT(YEAR FROM CURRENT_DATE - INTERVAL '2 month')
+          AND EXTRACT(MONTH FROM t.created_date) = EXTRACT(MONTH FROM CURRENT_DATE - INTERVAL '2 month')
+    """
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT", 5432),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
+        )
+        df = pd.read_sql_query(query, conn)
+        conn.close()
+        bin_list = df['processor_name'].tolist()
+        return bin_list
+    except Exception as e:
+        return []
+
